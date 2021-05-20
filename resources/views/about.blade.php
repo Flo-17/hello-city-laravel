a3e3a042ffcfc2ac2370f00e0369eb0b20f73b09
Flo <cammarata.florian@hotmail.com>
2021-05-20 14:41:53 +0200

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
     </head>
     <body>
-        <h1> Hello from Belgique ! </h1>
+        <p> Built with &hearts; by Florian. </p>
 
-        <p> It's currently {{ date('h:i A') }} </p>
+        <a href="/">Revenir à la page d'accueil</a>
 
         <footer>
-            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="about-us">About Us</a></p>
+            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
         </footer>    
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about-us', function () {
+    return view('about');
+});
+
+
