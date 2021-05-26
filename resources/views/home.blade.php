a0205f565c12810577d9d0e5d506815da6c3803e
Flo <cammarata.florian@hotmail.com>
2021-05-26 14:54:07 +0200

    Deploiement sur Heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <img src=" {{ asset('/Images/Drapeau_Belge.jpg') }} "  alt="Belgian Flag" class="mt-12 rounded h-44">
-    <h1 class="mt-5 mb-5 text-3xl sm:text-5xl font-semibold text-indigo-600"> Hello from Belgique ! </h1>
+    <h1 class="mt-5 mb-5 text-3xl sm:text-5xl font-semibold text-indigo-600"> Hello from Belgium ! </h1>
 
     <p class="text-lg text-gray-800"> It's currently {{ date('h:i A') }} </p> 
 @endsection
