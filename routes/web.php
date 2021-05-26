44fe065116a39ed975a1ee4c9583654b55f29a55
Flo <cammarata.florian@hotmail.com>
2021-05-26 14:51:41 +0200

    Modif dossier Pages

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/home');
+    return view('home');
 })->name('home');
 
 Route::get('/about-us', function () {
-    return view('pages/about');
+    return view('about');
 })->name('about');
 
 
